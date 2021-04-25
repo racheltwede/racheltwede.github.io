0790947a3b548aeee6b9e56234a71f5802828aef
RachelTwede <rtwede@byui.edu>
2021-04-24 18:13:25 -0600

    Filled out html, added CSS and JS

---
+++ code.js
@@ -0,0 +1,3 @@
+let currentDate = new Date;
+document.querySelector("#currentYear").innerHTML = currentDate.getFullYear();
+document.getElementById("lastModified").innerHTML = document.lastModified;
