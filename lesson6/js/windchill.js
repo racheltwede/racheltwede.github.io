35160d8b26cee6499f5f98775d3c98f41b9f5355
RachelTwede <rtwede@byui.edu>
2021-06-05 16:07:12 -0600

    finished gallery page css

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,14 +1,9 @@
-let tempF = document.querySelector("#temp").innerHTML;
-let windSpeed = document.querySelector("#wind").innerHTML;
-if (tempF > 50) {
-    document.querySelector("#chill").innerHTML = "n/a";
+let tempF = parseFloat(document.querySelector("#temp").textContent);
+let windSpeed = parseFloat(document.querySelector("#wind").textContent);
+if (windSpeed > 3 && tempF <= 50) {
+    let windChill = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * tempF * Math.pow(windSpeed, 0.16));
+    document.querySelector("#chill").innerHTML = Math.round(windChill) + "&#176;F";
 }
 else {
-    if (windSpeed < 3) {
-        document.querySelector("#chill").innerHTML = "n/a";
-    }
-    else {
-        let windChill = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * tempF * Math.pow(windSpeed, 0.16));
-        document.querySelector("#chill").innerHTML = Math.round(windChill) + "&#176;F";
-    }
+    document.querySelector("#chill").textContent = "n/a";
 }
