cd69cbdc464878f4f6c8b9f9633f5fde003e533f
RachelTwede <rtwede@byui.edu>
2021-06-19 23:43:10 -0600

    js and basic css for weather site main page

--- lesson9/js/main.js
+++ lesson9/js/main.js
@@ -1,29 +1,43 @@
 'use strict';
 
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const towns = jsonObject["towns"];
+        const weatherTowns = towns.filter(town => town.name == "Soda Springs" || town.name == "Fish Haven" || town.name == "Preston");
     
+        for (let i = 0; i < weatherTowns.length; i++) {
+            let blurb = document.createElement("div");
+            let h3 = document.createElement('h3');
+            let mottoP = document.createElement("p");
+            let image = document.createElement("img");
+            let yearP = document.createElement("p");
+            let populationP = document.createElement("p");
+            let rainP = document.createElement("p");
+
+            h3.textContent = weatherTowns[i].name;
+            mottoP.textContent = weatherTowns[i].motto;
+            image.setAttribute("src", weatherTowns[i].photo);
+            image.setAttribute("alt", "A photo of " + weatherTowns[i].name);
+            yearP.textContent = "Founded in " + weatherTowns[i].yearFounded;
+            populationP.textContent = "Population: " + weatherTowns[i].currentPopulation;
+            rainP.textContent = "Yearly rainfall: " + weatherTowns[i].averageRainfall + " in";
+
+            blurb.appendChild(h3);
+            blurb.appendChild(mottoP);
+            blurb.appendChild(image);
+            blurb.appendChild(yearP);
+            blurb.appendChild(populationP);
+            blurb.appendChild(rainP);
+
+            document.querySelector(".town-info").appendChild(blurb);
+        }
     
     /*const prophets = jsonObject['prophets'];
     for (let i = 0; i < prophets.length; i++ ) {
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let birthdayPara = document.createElement('p');
-        let birthplacePara = document.createElement('p');
-        let image = document.createElement('img');
-
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        birthdayPara.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-        birthplacePara.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-        image.setAttribute('src', prophets[i].imageurl);
-        image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + (i + 1));
-
-
         card.appendChild(h2);
         card.appendChild(birthdayPara);
         card.appendChild(birthplacePara);
