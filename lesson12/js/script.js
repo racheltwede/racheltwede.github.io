348be48a1b570af4edb1d320abf0e0901974af2d
RachelTwede <rtwede@byui.edu>
2021-07-12 14:12:46 -0600

    added social images

---
+++ lesson12/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function closeAlert() {
+    
+}
+
+function toggleMenu() {
+    document.querySelector("#nav-menu").classList.toggle("hidden");
+}
+
+function toggleSocial() {
+
+}
