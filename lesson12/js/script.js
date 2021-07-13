f9c0a1e8b3606efc1e151fd1d8a020aa0206d0ae
RachelTwede <rtwede@byui.edu>
2021-07-12 20:38:47 -0600

    worked on home page for commerce site

--- lesson12/js/script.js
+++ lesson12/js/script.js
@@ -9,5 +9,17 @@ function toggleMenu() {
 }
 
 function toggleSocial() {
-
+    document.querySelector("#social").classList.toggle("hidden");
 }
+
+//updates copyright year
+let now = new Date();
+document.querySelector(".copyright-year").innerHTML = now.getFullYear();
+
+//last modified date
+let modified = new Date(document.lastModified);
+const options = {year: 'numeric', month: 'short', day: 'numeric'}
+let intlModified = new Intl.DateTimeFormat("en-us", options).format(
+    modified
+);
+document.querySelector(".modified").innerHTML = intlModified;
