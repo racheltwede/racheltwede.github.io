b5a5b0a0f806b768023ac6db37356ba69d5dfd78
RachelTwede <rtwede@byui.edu>
2021-07-21 23:27:02 -0600

    rename (file copied)

---
+++ oremcommerce/js/directory.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const requestURL = "https://raw.githubusercontent.com/RachelTwede/racheltwede.github.io/master/lesson12/json/companies.json";
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const companies = jsonObject["companies"];
+    for (let i = 0; i < companies.length; i++ ) {
+        let card = document.createElement("section");
+        let h3 = document.createElement("h3");
+        let logo = document.createElement("img");
+        let contactInfo = document.createElement("div")
+        let phone = document.createElement("p");
+        let phoneSpan = document.createElement("span");
+        let address = document.createElement("p");
+        let addressSpan = document.createElement("span");
+        let website = document.createElement("p");
+        let websiteSpan = document.createElement("span");
+        let websiteLink = document.createElement("a");
+
+        h3.textContent = companies[i].name;
+        logo.setAttribute("src", companies[i].logo);
+        logo.setAttribute("alt", companies[i].name + " " + logo);
+        if(companies[i].name == "Utah Transit Authority" || companies[i].name == "Apple Spice") {
+          logo.classList.add("small-logo");
+        }
+        if(companies[i].name != "Kids on the Move" && companies[i].name != "Apple Spice" && companies[i].name != "Utah Transit Authority") {
+          logo.classList.add("med-logo");
+        }
+
+        phoneSpan.textContent = "phone";
+        addressSpan.textContent = "location_on";
+        websiteSpan.textContent = "web";
+        phoneSpan.classList.add("material-icons");
+        addressSpan.classList.add("material-icons");
+        websiteSpan.classList.add("material-icons");
+
+        websiteLink.textContent = companies[i].website;
+        websiteLink.setAttribute("href", companies[i].sitelink);
+
+        phone.appendChild(phoneSpan);
+        address.appendChild(addressSpan);
+        website.appendChild(websiteSpan);
+        website.appendChild(websiteLink);
+
+        phone.innerHTML = phone.innerHTML + companies[i].phone;
+        address.innerHTML = address.innerHTML + companies[i].address;
+
+        card.appendChild(h3);
+        card.appendChild(logo);
+        card.appendChild(contactInfo);
+        contactInfo.appendChild(phone);
+        contactInfo.appendChild(address);
+        contactInfo.appendChild(website);
+        contactInfo.classList.add("contact-info");
+
+
+        document.querySelector("#directory").appendChild(card);
+    }
+  });
+
+  function displayList() {
+    document.querySelector("#directory").classList.remove("directory-grid");
+  }
+  
+  function displayGrid() {
+    document.querySelector("#directory").classList.add("directory-grid");
+  }
---
+++ oremcommerce/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function toggleMenu() {
+    document.querySelector("#nav-ul").classList.toggle("hidden");
+}
+
+function toggleSocial() {
+    document.querySelector("#social").classList.toggle("hidden");
+    document.querySelector("#more").classList.toggle("hidden");
+    document.querySelector("#less").classList.toggle("hidden");
+}
+
+//updates copyright year
+let now = new Date();
+document.querySelector(".copyright-year").innerHTML = now.getFullYear();
+
+//last modified date
+let modified = new Date(document.lastModified);
+const options = {year: 'numeric', month: 'short', day: 'numeric'}
+let intlModified = new Intl.DateTimeFormat("en-us", options).format(
+    modified
+);
+document.querySelector(".modified").innerHTML = intlModified;
